79809dacc6a6b699a0a42aaa830fe4da8f7218c0
Chiheb <chiheb.z@datainsights.de>
2023-05-06 01:19:36 +0200

    remove dev log

--- pages/1_❄️_Sflake-Sandbox.py
+++ pages/1_❄️_Sflake-Sandbox.py
@@ -15,7 +15,6 @@ if 'authenticated' in st.session_state:
                            st.session_state.account,
                            st.session_state.warhouse)
         logout()
-        st.write(st.session_state)
         if 'check_status' not in st.session_state:
             st.session_state['check_status'] = None
             check_status = check_system(user_cnx)
